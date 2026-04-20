1f5eaa4fa2df3b29164ae985e05c2a2e3e42b236
Sneha <snehalucky@gmail.com>
2026-04-20 17:14:12 +0530

    Added Day5 DivideByZero exception handling program

---
+++ Day5-DivideByZeo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int a = 10;
+            int b = 0;
+
+            int result = a / b;
+
+            Console.WriteLine(result);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine("Done");
+        }
+    }
+}
