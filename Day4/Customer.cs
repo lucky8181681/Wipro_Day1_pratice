f63eae4d4d4baae41fb4825e0ec25a5cd2e5077e
Sneha <snehalucky@gmail.com>
2026-04-20 11:44:32 +0530

    Add Day4

---
+++ Day4/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4
+{
+    internal class Customer
+    {
+        public int Id;
+        public string Name;
+
+        public Customer(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
